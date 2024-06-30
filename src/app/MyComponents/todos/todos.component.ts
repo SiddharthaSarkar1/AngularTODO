605867e6a005700964f80df4e18009fb20082480
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-06-30 22:45:54 +0530

    minor updates

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -4,43 +4,40 @@ import { Todo } from 'src/app/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-
   localItem: string | null;
   todos!: Todo[];
-  constructor() { 
-    this.localItem = localStorage.getItem("todos");
-    
-    if(this.localItem == null){
+  constructor() {
+    this.localItem = localStorage.getItem('todos');
+
+    if (this.localItem == null) {
       this.todos = [];
-    }else{
+    } else {
       this.todos = JSON.parse(this.localItem);
     }
-   }
-
-  ngOnInit(): void {
   }
 
-  deleteTodo(todo:Todo){
+  ngOnInit(): void {}
+
+  deleteTodo(todo: Todo) {
     console.log(todo);
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
-    localStorage.setItem("todos", JSON.stringify(this.todos));//saving this to the local storage
+    localStorage.setItem('todos', JSON.stringify(this.todos)); //saving this to the local storage
   }
 
-  addTodo(todo:Todo){
+  addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(this.todos));//saving this to the local storage
+    localStorage.setItem('todos', JSON.stringify(this.todos)); //saving this to the local storage
   }
 
-  todoMarkasDone(todo:Todo){
+  todoMarkasDone(todo: Todo) {
     console.log(todo);
     const index = this.todos.indexOf(todo);
     this.todos[index].active = false;
-    localStorage.setItem("todos", JSON.stringify(this.todos));//saving this to the local storage
+    localStorage.setItem('todos', JSON.stringify(this.todos)); //saving this to the local storage
   }
-
 }
